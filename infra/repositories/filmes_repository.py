06256d605aa8fe571d37137e2007dafcefeb713c
Igor Barreto <igorbarreto1993@hotmail.com>
2022-10-22 19:33:22 -0300

    Trabalhando com exceções e como trata-las

--- infra/repositories/filmes_repository.py
+++ infra/repositories/filmes_repository.py
@@ -10,16 +10,24 @@ class FilmesRepository:
 
     def insert_one(self, titulo: str, ano: int, genero: str) -> None:
         with DBConectionHandler() as db:
-            novo_filme = Filmes(titulo=titulo, ano=ano, genero=genero)
-            db.session.add(novo_filme)
-            db.session.commit()
-            db.session.refresh(novo_filme)
-            return novo_filme
+            try:
+                novo_filme = Filmes(titulo=titulo, ano=ano, genero=genero)
+                db.session.add(novo_filme)
+                db.session.commit()
+                db.session.refresh(novo_filme)
+                return novo_filme
+            except Exception as e:
+                db.session.rollback()
+                raise e
 
     def delete_one(self, titulo):
         with DBConectionHandler() as db:
-            db.session.query(Filmes).filter(Filmes.titulo == titulo).delete()
-            db.session.commit()
+            try:
+                db.session.query(Filmes).filter(Filmes.titulo == titulo).delete()
+                db.session.commit()
+            except Exception as e:
+                db.session.rollback()
+                raise e
 
     def update_one(self, titulo: str, ano: str):
         with DBConectionHandler() as db:
