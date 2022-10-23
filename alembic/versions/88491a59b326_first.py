9fc333ab6863b328c166699def39471f265e1f76
Igor Barreto <igorbarreto1993@hotmail.com>
2022-10-22 22:06:29 -0300

    Adicionando a lib alembic para gerenciamento de migracoes

---
+++ alembic/versions/88491a59b326_first.py
@@ -0,0 +1,24 @@
+"""First
+
+Revision ID: 88491a59b326
+Revises: 
+Create Date: 2022-10-22 22:04:05.157054
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '88491a59b326'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
